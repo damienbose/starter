5965b1e965f93207f14c2de2a9dcc22d9d9779a3
Damien Bose <damienbose3@gmail.com>
2024-08-14 09:32:31 +0530

    better lazy git plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,7 +12,7 @@ if vim.g.vscode then
     -- Lazy Git
     map("n", "<leader>g", function()
         local code = require('vscode')
-        code.action('lazygit.openLazygit')
+        code.action('lazygit-vscode.toggle')
     end, { desc = "Lazy git (cwd)" })
     unmap("n", "<leader>gg")
     unmap("n", "<leader>gG")
